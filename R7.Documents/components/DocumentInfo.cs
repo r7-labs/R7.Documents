2696db8d5043b90c753a93b9598de466b5738de9
roman-yagodin <roman.yagodin@gmail.com>
2014-06-27 16:48:22 +0400

    Add PetaPOCO attributes

--- R7.Documents/components/DocumentInfo.cs
+++ R7.Documents/components/DocumentInfo.cs
@@ -22,6 +22,9 @@ using System;
 using System.Configuration;
 using System.Data;
 using DotNetNuke;
+using DotNetNuke.Data;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.ComponentModel.DataAnnotations;
 using DotNetNuke.Services.Localization;
 
 namespace R7.Documents
@@ -29,65 +32,112 @@ namespace R7.Documents
 	/// <summary>
 	/// Holds the information about a single document
 	/// </summary>
+	[TableName ("Documents")]
+	[PrimaryKey ("ItemId", AutoIncrement = true)]
+	[Scope ("ModuleId")]
 	public class DocumentInfo
 	{
+		/// <summary>
+		/// Empty default cstor
+		/// </summary>
+		public DocumentInfo ()
+		{
+		}
 
-		#region "Private Members"
+		#region Private Members
 
 		private int _clicks;
 
 		#endregion
-	
 
-		#region "Auto Implemented Properties"
 
-		// public properties
+		#region DB Properties
+
 		public int ItemId { get; set; }
+
 		public int ModuleId { get; set; }
+
 		public int CreatedByUserId { get; set; }
-		public string CreatedByUser { get; set; }
-		public System.DateTime CreatedDate { get; set; }
-		public string Url { get; set; }
-		public string Title { get; set; }
-		public string Category { get; set; }
-		public int Size { get; set; }
-		public string ContentType { get; set; }
-		public bool TrackClicks { get; set; }
-		public bool NewWindow { get; set; }
-		public int OwnedByUserId { get; set; }
-		public string OwnedByUser { get; set; }
+
 		public int ModifiedByUserId { get; set; }
-		public string ModifiedByUser { get; set; }
+
+		public DateTime CreatedDate { get; set; }
+
 		public DateTime ModifiedDate { get; set; }
+
+		public string Url { get; set; }
+
+		public string Title { get; set; }
+
+		public string Category { get; set; }
+
+		public int OwnedByUserId { get; set; }
+
 		public int SortOrderIndex { get; set; }
+
 		public string Description { get; set; }
+
 		public bool ForceDownload { get; set; }
+
 		#endregion
 
+		#region External properties
 
+		[ReadOnlyColumn]
+		public bool TrackClicks { get; set; }
 
+		[ReadOnlyColumn]
+		public string ContentType { get; set; }
 
+		[ReadOnlyColumn]
+		public bool NewWindow { get; set; }
 
-		#region "Custom Properties"
+		[ReadOnlyColumn]
+		public int Size { get; set; }
 
+		[ReadOnlyColumn]
+		public string CreatedByUser { get; set; }
+
+		[ReadOnlyColumn]
+		public string OwnedByUser { get; set; }
+
+		[ReadOnlyColumn]
+		public string ModifiedByUser { get; set; }
+
+		#endregion
+		
+		#region Custom properties
+		
 		/// <summary>
 		/// Gets the size of the format.
 		/// </summary>
 		/// <value>The size of the format.</value>
-		public string FormatSize {
-			get {
-				try {
-					if (!DotNetNuke.Common.Utilities.Null.IsNull(Size)) {
-						if (Size > (1024 * 1024)) {
-							return String.Format("{0:#,##0.00} MB", Size / 1024 / 1024);
-						} else {
-							return String.Format("{0:#,##0.00} KB", Size / 1024);
+		[IgnoreColumn]
+		public string FormatSize
+		{
+			get
+			{
+				try
+				{
+					if (!Null.IsNull (Size))
+					{
+						if (Size > (1024 * 1024))
+						{
+							return String.Format ("{0:#,##0.00} MB", Size / 1024 / 1024);
 						}
-					} else {
-						return Localization.GetString("Unknown");
+						else
+						{
+							return String.Format ("{0:#,##0.00} KB", Size / 1024);
+						}
 					}
-				} catch {
-					return Localization.GetString("Unknown");
+					else
+					{
+						return Localization.GetString ("Unknown");
+					}
+				}
+				catch
+				{
+					return Localization.GetString ("Unknown");
 				}
 			}
 		}
@@ -96,20 +146,13 @@ namespace R7.Documents
 		/// Gets or sets the clicks.
 		/// </summary>
 		/// <value>The clicks.</value>
-		public int Clicks {
-			get {
-				if (_clicks < 0) {
-					return 0;
-				} else {
-					return _clicks;
-				}
-
-			}
+		[IgnoreColumn]
+		public int Clicks
+		{
+			get { return (_clicks < 0) ? 0 : _clicks; }
 			set { _clicks = value; }
 		}
+
 		#endregion
-
-
 	}
-
 }
