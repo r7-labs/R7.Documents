deb7aa04bf059ec9832d7939f19c63ae543f1617
roman-yagodin <roman.yagodin@gmail.com>
2015-02-03 12:36:12 +0400

    Set version to 1.0.6, update release notes

--- R7.Documents/lib/Properties/AssemblyInfo.cs
+++ R7.Documents/lib/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("R7.Solutions")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("")]
+[assembly: AssemblyProduct ("R7.Documents")]
+[assembly: AssemblyCopyright ("Roman M. Yagodin")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.5.*")]
+[assembly: AssemblyVersion ("1.0.6.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
