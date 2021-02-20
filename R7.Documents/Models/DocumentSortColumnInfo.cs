6ce92e529998bbb68a8f0078b595b6fa0d496304
roman-yagodin <roman.yagodin@gmail.com>
2021-02-20 14:29:20 +0300

    Move 2 more classes to .NET Standard library

---
+++ R7.Documents/Models/DocumentSortColumnInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R7.Documents.Models
+{
+    [Serializable]
+    public class DocumentsSortColumnInfo : IDocumentsSortColumn
+    {
+        public SortDirection Direction { get; set; } = SortDirection.Ascending;
+
+        public string ColumnName { get; set; }
+    }
+}
