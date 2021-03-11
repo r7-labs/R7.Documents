caba4a7805138f24ab59e8cb43b2bf6f8c7ec530
roman-yagodin <roman.yagodin@gmail.com>
2021-03-11 10:59:12 +0300

    Modernize VSCode tasks GH-113, allow to run tests app

---
+++ R7.Documents.Tests/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace R7.Documents.Tests
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            Console.WriteLine ("Hello, tests!");
+        }
+    }
+}
