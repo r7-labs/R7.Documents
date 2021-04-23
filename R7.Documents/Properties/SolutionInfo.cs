521590e7c81f1f6b028c00caaf236eaaf12e85b0
roman-yagodin <roman.yagodin@gmail.com>
2021-04-23 14:33:37 +0300

    Bump version

--- R7.Documents/Properties/SolutionInfo.cs
+++ R7.Documents/Properties/SolutionInfo.cs
@@ -1,8 +1,8 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyCompany ("R7.Labs")]
 [assembly: AssemblyProduct ("R7.Documents")]
 [assembly: AssemblyCopyright ("Roman M. Yagodin")]
 [assembly: AssemblyTrademark ("")]
-[assembly: AssemblyVersion ("1.13.0")]
-[assembly: AssemblyInformationalVersion ("1.13.0")]
+[assembly: AssemblyVersion ("1.13.1")]
+[assembly: AssemblyInformationalVersion ("1.13.1")]
